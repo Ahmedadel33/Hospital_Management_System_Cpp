a280ca73c58c254ad709065d356aa67e15cbcd20
unknown <ahmed.a.tantawi@gmail.com>
2026-03-08 15:24:32 +0200

    Resolve merge conflicts with upstream/main

---
+++ Hospital_Management_System_Cpp/Person.cpp
@@ -0,0 +1,52 @@
+using namespace std;
+#include "string"
+#include "Person.h"
+#include <iostream>
+
+Person::Person() {};
+
+Person::Person(string name, int id, int age, string gender, int phone, string email) {
+	m_name = name;
+	m_id = id;
+	m_age = age;
+	m_gender = gender;
+	m_phone = phone;
+	m_email = email;
+}
+
+void Person::set_m_name(string n) {
+	m_name = n;
+}
+string Person::get_name() {
+	return m_name;
+}
+void Person::set_m_id(int id) {
+	m_id = id;
+}
+int Person::get_m_id() {
+	return m_id;
+}
+void Person::set_m_age(int age) {
+	m_age = age;
+}
+int Person::get_m_age() {
+	return m_age;
+}
+void Person::set_m_gender(string gender) {
+	m_gender = gender;
+}
+string Person::get_gender() {
+	return m_gender;
+}
+void Person::set_m_phone(int phone) {
+	m_phone = phone;
+}
+int Person::get_m_phone(int phone) {
+	return m_phone;
+}
+void Person::set_m_email(string email) {
+	m_email = email;
+}
+string Person::get_email() {
+	return m_email;
+}
---
+++ Hospital_Management_System_Cpp/Person.h
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#include "string";
+
+class Person
+{
+protected:
+	string m_name;
+	int m_id;
+	int m_age;
+	string m_gender;
+	int m_phone;
+	string m_email;
+
+public:
+	Person();
+
+	 Person(string name, int id, int age, string gender, int phone, string email);
+
+	void set_m_name(string n);
+	string get_name();	
+	void set_m_id(int id);
+	int get_m_id();
+	void set_m_age(int age);
+	int get_m_age();
+	void set_m_gender(string gender);
+	string get_gender();
+	void set_m_phone(int phone);
+	int get_m_phone(int phone);	
+	void set_m_email(string email);
+	string get_email();
+
+};
